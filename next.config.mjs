9cf56dd71cfef2a69af34fe72b2efdbedb249f56
filipencus <filip.enculescu@gmail.com>
2024-05-28 23:10:11 +0200

    fix images finally

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const basePath = "portfolio-cv";
-
 const nextConfig = {
   output: "export", // <=== enables static exports
   reactStrictMode: true,
-  // images: {
-  //   loader: "custom",
-  //   loaderFile: "./src/utils/ImageLoader.js",
-  //   unoptimized: true,
-  //   path: `${basePath}/_next/image`,
-  // },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- src/components/pages/testimonials/page.tsx
+++ src/components/pages/testimonials/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import BarActions from "@/components/shared/page-actions/BarActions";
-import ImageLoader from "@/utils/ImageLoader";
 
 export default function Testimonials() {
   return (
@@ -13,7 +12,7 @@ export default function Testimonials() {
 
       <div className={styles.testimonialContainer}>
         <div className={styles.manager}>
-          <Image src="/israel.jpeg" width={100} height={100} alt="Israel" loader={ImageLoader} />
+          <Image src="/israel.jpeg" width={100} height={100} alt="Israel" />
           <div>
             Israel Illescas
             <br />
@@ -21,29 +20,18 @@ export default function Testimonials() {
             <p>Business Manager</p> 
             <br />
             <a href="https://www.linkedin.com/in/israelillescas" target="_blank">
-              <Image
-                src="/ln.png"
-                width={20}
-                height={20}
-                alt="linkedin profile"
-                loader={ImageLoader}
-              />
+              <Image src="/ln.png" width={20} height={20} alt="linkedin profile" />
             </a>
           </div>
         </div>
 
-        <Image
-          src="/recommendation.png"
-          width={600}
-          height={835}
-          alt="recommendation"
-          loader={ImageLoader}
-        />
+        <Image src="/recommendation.png" width={600} height={835} alt="recommendation" />
       </div>
 
       <br />
       <br />
       <BarActions></BarActions>
+      
     </>
   );
 }
