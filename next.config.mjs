b120d8966dca698363c57d3d26aa25a07c41f6d8
filipencus <filip.enculescu@gmail.com>
2024-05-28 19:03:13 +0200

    fix github images path

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "export", // <=== enables static exports
   // basePath: "/portfolio-cv",
   reactStrictMode: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
